be18af50acf36d9f518a25648b31c4b6c703f08d
Amir shiri <plus1319@outlook.com>
2020-01-02 12:08:05 +0330

    add mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mediator.Sample1;
+
+namespace Mediator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Sample 1
+            IAirTrafficControl mehrabadAirTrafficControl = new MehrabadAirTrafficControl();
+
+            AirplaneBase airbusAirplane = new AirbusAirplane(mehrabadAirTrafficControl);
+            AirplaneBase boeingAirplane = new BoeingAirplane(mehrabadAirTrafficControl);
+            AirplaneBase jetAirplane = new JetAirplane(mehrabadAirTrafficControl);
+
+            mehrabadAirTrafficControl.AddAirplanes(airbusAirplane, boeingAirplane,jetAirplane);
+
+            airbusAirplane.Send("Can we land right now ?");
+            Console.WriteLine("----------");
+
+            boeingAirplane.Send("No! We're landing, wait ...");
+            Console.WriteLine("----------");
+
+            jetAirplane.Send("I haven't reached yet ");
+            Console.WriteLine("----------");
+
+            // Demonstrate landing ...
+            Console.WriteLine("Boeing is landing ...");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine("----------");
+
+            boeingAirplane.Send("We landed.");
+            Console.WriteLine("----------");
+
+            airbusAirplane.Send("OK, We're going to land ...");
+            Console.WriteLine("----------");
+
+            boeingAirplane.Send("Good luck.");
+        }
+    }
+}
---
+++ Mediator/Sample1/IAirTrafficControl.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mediator.Sample1
+{
+    //mediator
+    public interface IAirTrafficControl
+    {
+        void AddAirplanes(params AirplaneBase[] airplanes);
+
+        void SendMessage(string message, AirplaneBase messageProducerAirplane);
+    }
+    // Concrete Class for mediator
+    public class MehrabadAirTrafficControl : IAirTrafficControl
+    {
+        readonly List<AirplaneBase> _airplanes = new List<AirplaneBase>();
+
+        public void AddAirplanes(params AirplaneBase[] airplanes)
+        {
+            _airplanes.AddRange(airplanes);
+        }
+
+        public void SendMessage(string message, AirplaneBase messageProducerAirplane)
+        {
+            List<AirplaneBase> otherAirplanes = _airplanes
+                .Where(airplane => airplane != messageProducerAirplane)
+                .ToList();
+
+            foreach (var airplane in otherAirplanes)
+                airplane.Notify(message);
+        }
+    }
+    //Colleague
+    public abstract class AirplaneBase
+    {
+        public abstract void Send(string message);
+
+        public abstract void Notify(string message);
+    }
+    // Concrete Class Colleague
+    public class AirbusAirplane : AirplaneBase
+    {
+        protected readonly IAirTrafficControl AirTrafficControl;
+
+        public AirbusAirplane(IAirTrafficControl airTrafficControl)
+        {
+            AirTrafficControl = airTrafficControl;
+        }
+
+        public override void Send(string message)
+        {
+            Console.WriteLine("Airbus airplane sends message: " + message);
+            AirTrafficControl.SendMessage(message, this);
+        }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Airbus airplane gets message: " + message);
+        }
+    }
+    // Concrete Class Colleague
+    public class BoeingAirplane : AirplaneBase
+    {
+        protected readonly IAirTrafficControl AirTrafficControl;
+
+        public BoeingAirplane(IAirTrafficControl airTrafficControl)
+        {
+            AirTrafficControl = airTrafficControl;
+        }
+
+        public override void Send(string message)
+        {
+            Console.WriteLine("Boeing airplane sends message: " + message);
+            AirTrafficControl.SendMessage(message, this);
+        }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Boeing airplane gets message: " + message);
+        }
+    }
+    // Concrete Class Colleague
+    public class JetAirplane : AirplaneBase
+    {
+        protected readonly IAirTrafficControl AirTrafficControl;
+
+        public JetAirplane(IAirTrafficControl airTrafficControl)
+        {
+            AirTrafficControl = airTrafficControl;
+        }
+
+        public override void Send(string message)
+        {
+            Console.WriteLine("Jet airplane sends message: " + message);
+            AirTrafficControl.SendMessage(message, this);
+        }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Jet airplane gets message: " + message);
+        }
+    }
+
+}
