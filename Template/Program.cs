559aa9429969e91089c6781ba5272ec6475d59ca
Amir shiri <plus1319@outlook.com>
2020-01-05 15:05:02 +0330

    state core 2.2

---
+++ Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
